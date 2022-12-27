91369f06c38f5cd929f96220bd324d0e909636e6
Your Name <you@example.com>
2022-12-27 22:53:19 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# -*- version: 3.11 -*-
+import time
+
+from discord_webhook import DiscordWebhook
+import pickle
+from youtube_channel_statistics import YTChannelStatistics
+import tomllib
+from dotenv import load_dotenv
+import os
+from dataclasses import dataclass, field
+
+
+def make_config_file(config_filepath: str) -> None:
+    """Creates the toml config file if it doesn't exist."""
+
+    if os.path.isfile(config_filepath):
+        print(f"{config_filepath!r} exists.")
+    else:
+        with open(config_filepath, 'w') as file:
+            base_file_data = (f"# Don't forget the double quotes in 'channel_id' because it's a string.\n"
+                              f"[channel_info]\n\tchannel_id = <FILL_HERE>\n\tbase_subs_count = <FILL_HERE>\n\n"
+                              f"[runtime]\n\tstep_value = <FILL_HERE>\n\tcheck_interval = <FILL_HERE>")
+            file.write(base_file_data)
+        quit(f"Please open {config_filepath!r} and fill in the fields.")
+
+
+@dataclass
+class LoadConfig:
+    """A class that loads variables from a toml config file."""
+    config_filepath: str
+
+    channel_id: str = None, field(init=False)
+    base_subs_count: int = None, field(init=False)
+    subs_diff_step_value: int = None, field(init=False)
+    check_interval: float = None, field(init=False)
+
+    def load_config(self) -> dict:
+        with open(self.config_filepath, 'rb') as file:
+            config_data: dict = tomllib.load(file)
+
+        return config_data
+
+    def __post_init__(self):
+        config_data = self.load_config()
+
+        self.channel_id = config_data['channel_info']['channel_id']
+        self.base_subs_count = config_data['channel_info']['base_subs_count']
+        self.subs_diff_step_value = config_data['runtime']['step_value']
+        self.check_interval = config_data['runtime']['check_interval']
+
+
+@dataclass
+class LoadEnv:
+    """A class that loads variables from a .env file."""
+    youtube_api_key: str = field(init=False)
+    discord_webhook_url: str = field(init=False)
+
+    def __post_init__(self):
+        load_dotenv()
+        self.youtube_api_key = os.getenv('YOUTUBE_API_KEY')
+        self.discord_webhook_url = os.getenv("DISCORD_WEBHOOK_URL")
+
+
+def has_changed(current_sub_count: int, step: int, filename: str) -> tuple[bool, int] | bool:
+    """Returns True if subcount in pickle is equal to currentsubvcount AND picklesubcount is greater than step"""
+
+    then_sub_count = read_diff(filename)
+
+    if not current_sub_count != then_sub_count:
+        return False
+    if then_sub_count > step:
+        return True
+
+
+def number_prettifier(n: int) -> str:
+    return f'{n:,}'.replace(',', ' ')
+
+
+def send_webhook(webhook_url: str, message: str) -> None:
+    """Sends a Discord webhook."""
+    webhook = DiscordWebhook(url=webhook_url, content=message)
+
+    try:
+        status_code = webhook.execute().status_code
+        print(f"Successfully sent webhook. Status code: {status_code}")
+    except Exception as e:
+        print(f"Failed to send webhook. Error: {e}")
+
+
+def webhook_message(channel: YTChannelStatistics, youtube_token: str, sub_diff: int):
+    return (f":clock1: <t:{get_current_timestamp()}:F> :clock1:\n"
+            f"-**{channel.name}** has roughly **{number_prettifier(channel.subscriber_count)}** subscribers.\n"
+            f"-That means **{channel.name}** lost roughly **{number_prettifier(sub_diff)}** subscribers!")
+
+
+def get_current_timestamp() -> int:
+    return int(time.time())
+
+
+def write_subs(current_subs: int, filename: str) -> None:
+    """writes the current subscribers in .pickle file."""
+
+    with open(filename, 'wb') as file:
+        pickle.dump(current_subs, file)
+
+
+def read_diff(filename: str) -> int:
+    """Reads the .pickle file that contains the difference of subscribers and returns it."""
+
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
+
+
+def main() -> None:
+    make_config_file(CONFIG_FILEPATH)
+    env = LoadEnv()
+    config = LoadConfig(CONFIG_FILEPATH)
+
+    channel = YTChannelStatistics(channel_id=config.channel_id, api_token=env.youtube_api_key)
+    write_subs(current_subs=channel.subscriber_count, filename=DIFFERENCE_PICKLE_FILENAME)
+
+    while True:
+        channel = YTChannelStatistics(channel_id=config.channel_id, api_token=env.youtube_api_key)
+        if has_changed(current_sub_count=channel.subscriber_count,
+                       step=config.subs_diff_step_value, filename=DIFFERENCE_PICKLE_FILENAME):
+            message = webhook_message(channel=channel, youtube_token=env.youtube_api_key,
+                                      sub_diff=config.subs_diff_step_value)
+            send_webhook(webhook_url=env.discord_webhook_url, message=message)
+        write_subs(current_subs=channel.subscriber_count, filename=DIFFERENCE_PICKLE_FILENAME)
+
+        time.sleep(config.check_interval)
+
+
+if __name__ == '__main__':
+    CONFIG_FILEPATH = 'config.toml'
+    DIFFERENCE_PICKLE_FILENAME = 'diff.pickle'
+    main()
+# norman = YTChannelStatistics()
---
+++ youtube_channel_statistics.py
@@ -0,0 +1,148 @@
+import requests
+
+
+class YTChannelStatistics:
+    """ Fetch the statistics of a given YouTube channel by its channelID """
+
+    @staticmethod
+    def number_prettifier(num: int | float | complex) -> str | None:
+        # 1000000 -> 1 000 000, 1000000.555555 -> 1 000 000.555555, 1000j -> 1 000j
+        if not isinstance(num, (int, float, complex)):
+            return None
+        return '{:,}'.format(num).replace(',', ' ')
+
+    def __init__(self, channel_id: str, api_token: str):
+        self._channel_id = channel_id
+        self._api_token = api_token
+        self.errors = []
+        self.subscriber_count = None
+        self.video_count = None
+        self.view_count = None
+        self.is_subscriber_count_hidden = None
+        self.name = None
+        self.description = None
+        self.keywords = None
+        self.country = None
+        self.banner_url = None
+
+        if not self._is_valid():
+            raise TypeError
+
+        self._parse_data_statistics()
+        self._parse_data_branding()
+
+    def _get_statistics_url(self) -> str:
+        """Return the YoutubeApi url."""
+        url = (
+            f"https://www.googleapis.com/youtube/v3/channels?id="
+            f"{self._channel_id}&key={self._api_token}&part=statistics"
+        )
+        return url
+
+    def _get_branding_url(self) -> str:
+        """Return the YoutubeApi url."""
+        url = (
+            f"https://www.googleapis.com/youtube/v3/channels?id="
+            f"{self._channel_id}&key={self._api_token}&part=brandingSettings"
+        )
+        return url
+
+    def _is_valid(self) -> bool:
+        """ Verify that 'channel_id' & 'api_token' are str && the length they should be. If not return False."""
+        if type(self._channel_id) != str or type(self._api_token) != str:
+            return False
+        elif len(self._channel_id) != 24 or len(self._api_token) != 39:
+            return False
+        else:
+            return True
+
+    def _fetch_data(self, url: str) -> dict | None:
+        """ Gets dict from the url with httpx.get() and json.load() methods."""
+        try:
+            site = requests.get(url)
+        except Exception as e:
+            print(f"Exception encountered: {str(e)}")
+            self.errors.append(e)
+            return None
+        else:
+            return site.json()
+
+    def _parse_data_statistics(self):
+        """ Get subscribers, views, video_count, is_sub_count_hidden. If error returns None"""
+        data = self._fetch_data(self._get_statistics_url())
+
+        if data is None:
+            return
+
+        try:
+            self.subscriber_count = int(data['items'][0]['statistics']['subscriberCount'])
+        except Exception as e:
+            print(f"Exception encountered: {str(e)}")
+            self.errors.append(e)
+        try:
+            self.view_count = int(data['items'][0]['statistics']['viewCount'])
+        except Exception as e:
+            print(f"Exception encountered: {str(e)}")
+            self.errors.append(e)
+        try:
+            self.video_count = int(data['items'][0]['statistics']['videoCount'])
+        except Exception as e:
+            print(f"Exception encountered: {str(e)}")
+            self.errors.append(e)
+        try:
+            self.is_subscriber_count_hidden = data['items'][0]['statistics']['hiddenSubscriberCount']
+        except Exception as e:
+            print(f"Exception encountered: {str(e)}")
+            self.errors.append(e)
+
+    def _parse_data_branding(self):
+        """ Get name, description, keywords, country, banner_url If error returns None"""
+
+        data = self._fetch_data(self._get_branding_url())
+
+        if data is None:
+            return
+
+        try:
+            self.name = data['items'][0]['brandingSettings']['channel']['title']
+        except Exception as e:
+            print(f"Exception encountered: {str(e)}")
+            self.errors.append(e)
+        try:
+            self.description = data['items'][0]['brandingSettings']['channel']['description'].strip('\n')
+        except Exception as e:
+            print(f"Exception encountered: {str(e)}")
+            self.errors.append(e)
+        try:
+            self.keywords = data['items'][0]['brandingSettings']['channel']['keywords']
+        except Exception as e:
+            print(f"Exception encountered: {str(e)}")
+            self.errors.append(e)
+        try:
+            self.country = data['items'][0]['brandingSettings']['channel']['country']
+        except Exception as e:
+            print(f"Exception encountered: {str(e)}")
+            self.errors.append(e)
+        try:
+            self.banner_url = data['items'][0]['brandingSettings']['image']['bannerExternalUrl']
+        except Exception as e:
+            print(f"Exception encountered: {str(e)}")
+            self.errors.append(e)
+
+    def __str__(self):
+        diagnostic = (
+            f"_channel_id: {self._channel_id}\n"
+            f"_api_token: {self._api_token}\n"
+            f"errors: {self.errors}\n"
+            f"subscriber_count: {self.subscriber_count}\n"
+            f"video_count: {self.video_count}\n"
+            f"view_count: {self.view_count}\n"
+            f"is_subscriber_count_hidden: {self.is_subscriber_count_hidden}\n"
+            f"name: {self.name}\n"
+            f"description: {self.description}\n"
+            f"keywords: {self.keywords}\n"
+            f"country: {self.country}\n"
+            f"banner_url: {self.banner_url}\n"
+        )
+
+        return diagnostic
